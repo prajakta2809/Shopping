11f02ccbc034190874cce7b247fdd3bd1569c9c4
Prajakta Digraje <prajaktadigraje13@gmail.com>
2023-10-13 00:58:40 +0530

    models and controllers

--- models/product.js
+++ models/product.js
@@ -1,13 +1,13 @@
 
-
-
+const fs=require('fs');
+const path=require=('path');
 module.exports= class Product{
     constructor(t){
         this.title=t;
     }
 
     save(){
-        products.push(this);
+        const p=path.join(path.dirname(process.mainModule.filename));
     }
 
     static fetchAll(){
