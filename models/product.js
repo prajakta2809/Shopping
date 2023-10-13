4af89b3e8d07affe290d53b228ee17396c09fd1d
Prajakta Digraje <prajaktadigraje13@gmail.com>
2023-10-13 22:43:31 +0530

    file system

--- models/product.js
+++ models/product.js
@@ -1,16 +1,39 @@
+const path=require('path');
+const p=path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+const getAddProductFromFile=(cb)=>{
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+           return cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }
+        
+    })
+}
 
 const fs=require('fs');
-const path=require=('path');
+const { fileLoader } = require('ejs');
 module.exports= class Product{
     constructor(t){
         this.title=t;
     }
 
     save(){
-        const p=path.join(path.dirname(process.mainModule.filename));
+       // const p=path.join(path.dirname(process.mainModule.filename),'data','products.json');
+       getAddProductFromFile(products=>{
+      // let products = [];
+       
+        products.push(this);
+        fs.writeFile(p,JSON.stringify(products),(err)=>{
+            console.log(err);
+        });
+       });
+        //fs.readFile(p,(err,fileContent)=>{});
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        getAddProductFromFile(cb);
+        
     }
 }
