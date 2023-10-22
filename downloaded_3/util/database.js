79c348999d067728b294e5826371d0bdda052083
Prajakta Digraje <prajaktadigraje13@gmail.com>
2023-10-22 21:31:56 +0530

    sequelize

--- downloaded_3/util/database.js
+++ downloaded_3/util/database.js
@@ -1,11 +1,16 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'TusharMinche@2003'
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user:'root',
+//     database:'node-complete',
+//     password:'TusharMinche@2003'
 
-})
+// })
 
-module.exports=pool.promise();
+// module.exports=pool.promise();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete','root','TusharMinche@2003',{dialect:'mysql',host:'localhost'});
+
+module.exports=sequelize;
